1396a3d5ed2a09788ff331a39521ef5df8b5dcf2
Isuru-rangana <isururangana2002@gmail.com>
2026-01-11 11:06:42 -1000

    added model files

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'active',
+        'category_id',
+    ];
+    protected $casts = [
+        'price' => 'decimal:2',
+        'active' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+   
+}
