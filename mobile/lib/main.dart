070e1d45139130e2e244267d7041462bf2f6cc15
Isuru-rangana <isururangana2002@gmail.com>
2026-01-11 10:25:50 -1000

    Initial commit: Laravel-Flutter product management system setup

---
+++ mobile/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'core/di/injection_container.dart' as di;
+import 'features/products/presentation/pages/product_form_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize dependency injection
+  await di.init();
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product Management',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ProductFormPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
